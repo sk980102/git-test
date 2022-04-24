2b45b3b4268d5b02c658ec244d9951e8839a0652
sk <sk@jjumpbear.com>
2022-04-24 15:58:33 +0800

    first commmit

---
+++ src/main/java/com/jjumpbear/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jjumpbear.git;
+
+/**
+ * @author sk
+ * @create 2022-04-24 15:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
